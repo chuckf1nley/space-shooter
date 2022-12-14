157d752d064770a71047530baa8dfd5ef010fa56
ben riffe <benriffe@gmail.com>
2022-12-13 19:44:50 -0700

    updated player movement trimmed code

--- Assets/scripts/player.cs
+++ Assets/scripts/player.cs
@@ -18,62 +18,61 @@ public class player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //if player positon
 
-     transform.position = new Vector3(0, 0, 0);
-
-
+        transform.position = new Vector3(0, 0, 0);
 
     }
 
     // Update is called once per frame
     void Update()
-
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
-
-        Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
-        transform.Translate(direction * _speed * Time.deltaTime);
-
-        if (transform.position.y >= 0)
         {
-            transform.position = new Vector3(transform.position.x, 0, 0);
-
+            CalculaateMovement();
         }
-        else if (transform.position.y <=-3.8f)
+            
+        void CalculaateMovement()
+           
+            {
+                float horizontalInput = Input.GetAxis("Horizontal");
+                float verticalInput = Input.GetAxis("Vertical");
+
+                Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
+
+                transform.Translate(direction * _speed * Time.deltaTime);
+
+                if (transform.position.y >= 0)
+                {
+                    transform.position = new Vector3(transform.position.x, 0, 0);
+
+                }
+                else if (transform.position.y <= -3.8f)
+                {
+                    transform.position = new Vector3(transform.position.x, -3.8f, 0);
+                }
+
+
+
+                if (transform.position.x > 11f)
+                {
+                    transform.position = new Vector3(-11f, transform.position.y, 0);
+                }
+                else if (transform.position.x < -11f)
+                {
+                    transform.position = new Vector3(11f, transform.position.y, 0);
+                }
+            }
+
+        //if (Input.GetKeyDown(KeyCode.Space))
         {
-            transform.position = new Vector3(transform.position.x, -3.8f, 0);
+        //Instantiate(_laserPrefab, transform.position, Quaternion.identify);
+        }
         }
     
 
-        if (transform.position.x > 11f)
-        {
-            transform.position = new Vector3(-11f, transform.position.y, 0);
-        }
-        else if (transform.position.x < -11f)
-    {
-            transform.position = new Vector3(11f, transform.position.y, 0);
-        }
-    }
-    //(
-    //CalculateMovement()
-
-    //if (Input.GetKeyDown(KeyCode.Space))
     //{
-    //Instantiate(_laserPrefab, transform.position, Quaternion.identify);
-    //}
-    //}
+            //transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -3.8f, 0), 0);
 
-    //}
-    //}
-    //CalculateMovement()
-
-   
-    //transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -3.8f, 0), 0);
+        }
 
 
-
-    //new Vector3(5, 0, 0) * 5 * real time
-
-}
+    
