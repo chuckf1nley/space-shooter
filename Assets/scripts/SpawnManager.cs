2be00c01ed058879a08f780a727df62a6c658699
ben riffe <benriffe@gmail.com>
2023-05-08 14:00:31 -0600

    workin on wave spwan system day 1

--- Assets/scripts/SpawnManager.cs
+++ Assets/scripts/SpawnManager.cs
@@ -15,10 +15,84 @@ public class SpawnManager : MonoBehaviour
     private float Range;
     private float Length;
 
+    public List<Enemy> enemies = new List<Enemy>();
+    public int currWave;
+    public int waveValue;
+    public List<GameObject> enemiesToSpawn = new List<GameObject>();
+
+    public Transform spawnLocation;
+    public int waveDuration;
+    public float waveTimer;
+    private float spawnInterval;
+    private float spawnTimer;
+
     // Start is called before the first frame update
     void Start()
     {
+        GenerateWave();
+    }
 
+    void FixedUpdate()
+    {
+        if (spawnTimer < -0)
+        {
+            if(enemiesToSpawn.Count >0)
+            {
+                Instantiate(enemiesToSpawn[0], spawnLocation.position, Quaternion.identity);
+                enemiesToSpawn.RemoveAt(0);
+                spawnTimer = spawnInterval;
+            }
+            else
+            {
+                waveTimer = 0;
+            }
+        }
+        else
+        {
+            spawnTimer -= Time.fixedDeltaTime;
+            waveTimer -= Time.fixedDeltaTime;
+        }
+    }
+
+    public void GenerateWave()
+    {
+        waveValue = currWave * 10;
+        GenerateEnemies();
+
+        spawnInterval = waveDuration / enemiesToSpawn.Count;
+        waveTimer = waveDuration;
+    }
+
+    public void GenerateEnemies()
+    {
+        List<GameObject> generatedEnemies = new List<GameObject>();
+        while (waveValue > 0)
+        {
+            int randEnemyId = UnityEngine.Random.Range(0, enemies.Count);
+            int randEnemyCost = enemies[randEnemyId].cost;
+
+            if (waveValue - randEnemyCost > -0)
+            {
+                generatedEnemies.Add(enemies[randEnemyId].enemyPrefab);
+                waveValue -= randEnemyCost;
+            }
+            else if(waveValue <= 0)
+            {
+                break;
+            }
+        
+        }
+        enemiesToSpawn.Clear();
+        enemiesToSpawn = generatedEnemies;
+    }
+
+
+    [System.Serializable]
+    public class Enemy
+    {
+        public GameObject enemyPrefab;
+        public int cost;
+    
     }
     public void StartSpawning()
     {
@@ -38,7 +112,7 @@ public class SpawnManager : MonoBehaviour
 
             _enemy.transform.parent = _enemyContainer.transform;
             Enemy _enemyScript = _enemy.GetComponent<Enemy>();
-            _enemyScript.SetID(randomEnemy);
+          // _enemyScript.SetID(randomEnemy);
 
             _enemy.transform.parent = _enemyContainer.transform;
             yield return new WaitForSeconds(5.0f);
