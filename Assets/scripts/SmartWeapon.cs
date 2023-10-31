5a4f32b4dfd3dc3bfeed240b58d428d34c628a1b
ben riffe <benriffe@gmail.com>
2023-10-31 13:38:49 -0600

    powerup collected day 2

--- Assets/scripts/SmartWeapon.cs
+++ Assets/scripts/SmartWeapon.cs
@@ -7,12 +7,11 @@ public class SmartWeapon : MonoBehaviour
     [SerializeField] private float _speed = 2f;
     [SerializeField] private float _chaseSpeed = 2.5f;
     private Player _player;
+    private GameObject Player;
+    private GameObject Projectile;
     private float _playerDistance;
     private float _interceptDistance;
     private bool _isPlayerAlive;
-
-    private Vector2 target;
-    private Vector2 position;
 
     void Start()
     {
@@ -24,10 +23,8 @@ public class SmartWeapon : MonoBehaviour
         {
             Debug.Log("player is null on smartweapon");
         }
-    //    target = new Vector2(0.0f, 0.0f);
-    //    position = gameObject.transform.position;
 
-       //Debug.Break();
+        //Debug.Break();
     }
 
     // Update is called once per frame
@@ -39,35 +36,24 @@ public class SmartWeapon : MonoBehaviour
             }
         //{
         float step = _speed * Time.deltaTime;
+        Projectile.transform.position = Vector3.MoveTowards(Projectile.transform.position, Player.transform.position, _speed);
 
-        // move sprite towards the target location
-        transform.position = Vector2.MoveTowards(transform.position, target, step);
     }
 
     //google search for unity movetowards 2d
 
-    void OnGUI()
-    {
-        Event currentEvent = Event.current;
-        Vector2 playerPos = new Vector2();
-        Vector2 point = new Vector2();
-
-        //// compute where the player is in world space
-        //playerPos.x = currentEvent.playerPostion.x;
-        //playerPos.y = cam.pixelHeight - currentEvent.playerPosition.y;
-        //point = cam.ScreenToWorldPoint(new Vector3(playerPos.x, playerPos.y, 0.0f));
-
-    }
-
+   
 
 
     public void Weapon()
     {
-            transform.Translate(Vector3.up * _speed * Time.deltaTime);
-            Vector3 direction = transform.position - _player.transform.position;
-            direction = direction.normalized;
+        // move sprite towards the target location
 
-            transform.Translate(direction * _chaseSpeed * Time.deltaTime);
+        transform.Translate(Vector3.up * _speed * Time.deltaTime);
+        //Vector3 direction = transform.position + _player.transform.position;
+        //direction = direction.normalized;
+
+        //transform.Translate(direction * _chaseSpeed * Time.deltaTime);
 
 
         if (transform.position.y > 9f)
