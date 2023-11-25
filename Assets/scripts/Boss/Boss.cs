01d33aae79e491b3a7730bdccf59db67799ed336
ben riffe <benriffe@gmail.com>
2023-11-25 13:03:18 -0700

    Boss day 3

--- Assets/scripts/Boss/Boss.cs
+++ Assets/scripts/Boss/Boss.cs
@@ -4,13 +4,28 @@ using UnityEngine;
 
 public class Boss : MonoBehaviour
 {
-    [SerializeField] private GameObject _weapon;
-
+    [SerializeField] private GameObject _weapon;    
+    [SerializeField] private float _speed = 3f;
+    [SerializeField] private AudioClip audioClip;
+    [SerializeField] private int _enemyID;
+    [SerializeField] private SpriteRenderer _spriteRenderer;
+    [SerializeField] private AudioClip _audioClip;
+    private float _positionX;
+    private float _fireRate = 2f;
+    private float _startX;
+    private int _bossHealth;
+    private Player _player;
+    private AudioSource _audioSource;
+    private Animator _enemyDeathAnim;
+    private SpawnManager _spawnManager;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        _spawnManager = GameObject.Find("Spawn Manager").GetComponent<SpawnManager>();
+        _audioSource = GetComponent<AudioSource>();
+        _audioClip = GetComponent<AudioClip>();
+
     }
 
     // Update is called once per frame
